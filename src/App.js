463114c6af561c26e65e551ce196b9a72c597463
shubhamdobhal05 <shubhamdobhal05@gmail.com>
2022-05-20 22:44:26 +0530

    routes added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ import Kids from "./component/Kids";
 import MensDetails from './component/MensDetails';
 import WomensDetails from './component/WomensDetails';
 import KidsDetails from './component/KidsDetails';
+import Login from "./component/Login";
+import Register from "./component/Register";
+import Cart from "./component/Cart";
+import Checkout from "./component/Checkout"
 
 
 function App() {
@@ -21,6 +25,10 @@ function App() {
         <Route path="/mens/:id" element = {<MensDetails/>}/>
         <Route path="/womens/:id" element = {<WomensDetails/>}/>
         <Route path="/kids/:id" element = {<KidsDetails/>}/>
+        <Route path='/login' element = {<Login/>}/>
+        <Route path='/register' element = {<Register/>}/>
+        <Route path='/cart' element = {<Cart/>}/>
+        <Route path='/checkout' element = {<Checkout/>}/>
 
         
       </Routes>
